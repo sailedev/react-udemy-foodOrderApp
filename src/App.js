476a01a2a2ceef0f63e6709c1318073687388303
SaileFromEU <eliasskaugeeriksen@gmail.com>
2022-07-25 01:45:20 +0200

    done

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+import Header from './components/Layout/Header';
+import Meals from './components/Meals/Meals';
+import Cart from './components/Cart/Cart';
+import CartProvider from './store/CartProvider';
+
+function App() {
+  const [cartIsShown, setCartIsShown] = useState(false);
+
+  const showCardHandler = () => {
+    setCartIsShown(true);
+  };
+
+  const hideCardHandler = () => {
+    setCartIsShown(false);
+  };
+
+  return (
+    <CartProvider>
+      {cartIsShown && <Cart onHideCart={hideCardHandler} />}
+      <Header onShowCard={showCardHandler} />
+      <main>
+        <Meals />
+      </main>
+    </CartProvider>
+  );
+}
+
+export default App;
---
+++ src/components/Layout/Header.js
@@ -0,0 +1,21 @@
+import React, { Fragment } from 'react';
+
+import mealsImage from '../../assets/meals.jpg';
+import classes from './Header.module.css';
+import HeaderCartButton from './HeaderCartButton';
+
+const Header = (props) => {
+  return (
+    <Fragment>
+      <header className={classes.header}>
+        <h1>ReactMeals</h1>
+        <HeaderCartButton onClick={props.onShowCard} />
+      </header>
+      <div className={classes['main-image']}>
+        <img alt='A table full og delicious food!' src={mealsImage} />
+      </div>
+    </Fragment>
+  );
+};
+
+export default Header;
